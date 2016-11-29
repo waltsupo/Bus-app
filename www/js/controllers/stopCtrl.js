82948ef195b69b96cf06dc1c807cdcabe275ead6
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2016-11-29 13:23:23 +0200

    Add tabs to stop-view
    Ionic tabs didnt work so I have to make my own.

--- www/js/controllers/stopCtrl.js
+++ www/js/controllers/stopCtrl.js
@@ -4,6 +4,22 @@
 
     app.controller('stopCtrl', function ($scope, $rootScope) {
 
+        var openMenu = 0;
+
+        $scope.isOpen = function(number) {
+
+            if (number == openMenu) {
+                return true;
+            }
+
+            return false;
+        };
+
+        $scope.openMenu = function(number) {
+
+            openMenu = number;
+        };
+
         $scope.name = $rootScope.stop.name + " (" + $rootScope.stop.shortName + ")";
     })
 })();
