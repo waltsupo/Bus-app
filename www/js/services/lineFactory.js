d84cb4ad5dceb7957c2637a36737cb3e664ffe14
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2016-12-01 10:06:40 +0200

    Modify lineFactory to use Utils to decrease code duplicates

--- www/js/services/lineFactory.js
+++ www/js/services/lineFactory.js
@@ -2,24 +2,16 @@
 
     var app = angular.module('Bus-app');
 
-    app.factory('Lines', ["$resource", function ($resource) {
+    app.factory('Lines', ["$resource", function ($resource, Utils) {
 
+        // Default path to get information about lines
         var path = "http://data.itsfactory.fi/journeys/api/1/lines";
 
         var lines = {};
 
         lines.getLines = function () {
 
-            return new Promise(function(resolve, reject) {
-
-                var resource = $resource(path);
-
-                resource.get({}, function (res) {
-                    resolve(res);
-                }, function () {
-                    reject();
-                });
-            });
+            return Utils.getResponse(path);
         };
 
         return lines;
